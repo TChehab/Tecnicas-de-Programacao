4e028e0698fa55a735da74107a8a074dfa0b0367
TChehab <127149294+TChehab@users.noreply.github.com>
2023-04-24 02:29:41 -0300

    Update qst_insertionsort.c

--- qst_insertionsort.c
+++ qst_insertionsort.c
@@ -23,9 +23,11 @@ void insertionSort(ListaAlunos* lista) {
     for (i = 1; i < lista->tamanho; i++) {
         chave = lista->inicio->prox;
         atual = chave->ant;
-        for (j = i - 1; j >= 0 && atual->matricula > chave->matricula; j--) {
+        for (j = i - 1; j >= 0 && atual->media > chave->media; j--) {
             atual->prox = chave->prox;
-            chave->prox->ant = atual;
+            if (chave->prox != NULL) {
+                chave->prox->ant = atual;
+            }
             atual->ant->prox = chave;
             chave->ant = atual->ant;
             atual->ant = chave;
@@ -33,11 +35,12 @@ void insertionSort(ListaAlunos* lista) {
             if (atual == lista->inicio) {
                 lista->inicio = chave;
             }
-            chave = atual;
-            atual = chave->ant;
+            atual = chave;
+            chave = atual->prox; 
         }
     }
 }
+
 
 int main() {
     ListaAlunos* lista = (ListaAlunos*) malloc(sizeof(ListaAlunos));
@@ -46,13 +49,32 @@ int main() {
 
     // Para adicionar algum aluno na lista, seguir o modelo:
     Aluno* a1 = (Aluno*) malloc(sizeof(Aluno));
-    a1->matricula = 2;
-    strcpy(a1->nome, "Aluno 1");
-    a1->media = 8.5;
+    a1->matricula = 2211194;
+    strcpy(a1->nome, "Tiago Chehab");
+    a1->media = 7;
     a1->prox = NULL;
     a1->ant = NULL;
     lista->inicio = a1;
     lista->tamanho++;
+    
+    Aluno* a2 = (Aluno*) malloc(sizeof(Aluno));
+    a2->matricula = 2298474;
+    strcpy(a2->nome, "Gabriel Brandao");
+    a2->media = 9;
+    a2->prox = NULL;
+    a2->ant = a1;
+    a1->prox = a2;
+    lista->tamanho++;
+
+    Aluno* a3 = (Aluno*) malloc(sizeof(Aluno));
+    a3->matricula = 2236187;
+    strcpy(a3->nome, "Enzo Filippelli");
+    a3->media = 4.5;
+    a3->prox = NULL;
+    a3->ant = a2;
+    a2->prox = a3;
+    lista->tamanho++;
+
 
     // Para ordenar a lista usando o insertion sort:
     insertionSort(lista);
